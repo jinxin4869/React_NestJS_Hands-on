b966b3c117f17be5da1c5854bd432b51ef38fd9f
jinxin4869 <jinxin10244869@gmail.com>
2025-09-22 08:59:40 +0900

    ユーザーモジュールの読み込みができない問題の解消

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,11 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { UserModule } from '../user/user.module';
 
-@Global() // どのモジュールからでも使えるように Global にする
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
-  imports: [UserModule],
 })
 export class PrismaModule {}
